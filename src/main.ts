caceaec3984826cd4d7c0e0f43fcc798eee67dde
yalbassuny <yasmeen.albassuny.ext@orange.com>
2023-04-23 06:17:09 +0200

    Auto-generated commit [ci skip]

--- src/main.ts
+++
@@ -1,5 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import {LaptopsModule} from "./components/laptops/laptops.module";
-
-platformBrowserDynamic().bootstrapModule(LaptopsModule)
-  .catch(err => console.error(err));
